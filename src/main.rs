0a7d4d554c6ab7c94c6e66cbab6a679ae5c99765
Matias <matias.lindberg@aalto.fi>
2025-01-15 20:07:03 +0200

    basic

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::collections::VecDeque;
 
 const SIZE: usize = 32;
 const CELL: f32 = 20.0;
-const OFFSET: f32 = 40.0;
+const OFFSET_Y: f32 = 60.0;
+const OFFSET_X: f32 = 40.0;
 type Point = (usize, usize);
 
 #[derive(PartialEq)]
@@ -20,18 +21,25 @@ struct ScoreSnake {
 }
 
 struct Fruit {
-    position: (usize, usize),
-    points: usize,
+    pos: Point,
+    draw_pos: (f32, f32),
+    pts: usize,
 }
 
 impl Fruit {
     fn change_points(&mut self, points: usize) {
-        self.points = points;
+        self.pts = points;
     }
     fn change_position(&mut self, pos: Point) {
-        self.position = pos;
+        self.pos = pos;
+        self.draw_pos = (
+            OFFSET_X + CELL * self.pos.1 as f32,
+            OFFSET_Y + CELL * self.pos.0 as f32,
+        );
     }
-    fn draw_fruit() {}
+    fn draw_fruit(&self) {
+        draw_rectangle(self.draw_pos.0, self.draw_pos.1, CELL, CELL, RED);
+    }
 }
 
 struct Snake {
@@ -52,7 +60,6 @@ impl Snake {
             return;
         }
         self.dir = new_dir;
-        println!("Changed direction");
     }
     fn move_snake(&mut self) -> Option<()> {
         match &self.dir {
@@ -62,57 +69,63 @@ impl Snake {
             Direction::RIGHT => self.head.1 = (self.head.1 + SIZE + 1) % SIZE,
         }
         if self.snake.contains(&self.head) {
+            self.moves.push(self.head.clone());
+            self.snake.push_front(self.head.clone());
             None
         } else {
             self.moves.push(self.head.clone());
             self.snake.push_front(self.head.clone());
-            self.snake.pop_back();
             Some(())
         }
     }
 
-    fn add_points(&mut self, points: usize) {
+    fn add_points(&mut self, points: &usize) {
         self.pts += points;
     }
 
     fn draw_snake(&self) {
-        draw_rectangle(
-            OFFSET + CELL * self.head.1 as f32,
-            OFFSET + CELL * self.head.0 as f32,
-            CELL,
-            CELL,
-            GREEN,
-        );
         for p in self.snake.iter().skip(1) {
             draw_rectangle(
-                OFFSET + CELL * p.1 as f32,
-                OFFSET + CELL * p.0 as f32,
+                OFFSET_X + CELL * p.1 as f32,
+                OFFSET_Y + CELL * p.0 as f32,
                 CELL,
                 CELL,
                 DARKGREEN,
             );
         }
+        draw_rectangle(
+            OFFSET_X + CELL * self.head.1 as f32,
+            OFFSET_Y + CELL * self.head.0 as f32,
+            CELL,
+            CELL,
+            GREEN,
+        );
     }
 }
 
-fn draw_map() {
+struct Line {
+    start: (f32, f32),
+    end: (f32, f32),
+}
+
+fn build_grid() -> Vec<Line> {
+    let mut grid: Vec<Line> = Vec::new();
     for i in 0..SIZE + 1 {
-        draw_line(
-            OFFSET + CELL * i as f32,
-            OFFSET,
-            OFFSET + CELL * i as f32,
-            OFFSET + CELL * SIZE as f32,
-            1.0,
-            DARKGRAY,
-        );
-        draw_line(
-            OFFSET,
-            OFFSET + CELL * i as f32,
-            OFFSET + CELL * SIZE as f32,
-            OFFSET + CELL * i as f32,
-            1.0,
-            DARKGRAY,
-        );
+        grid.push(Line {
+            start: (OFFSET_X + CELL * i as f32, OFFSET_Y),
+            end: (OFFSET_X + CELL * i as f32, OFFSET_Y + CELL * SIZE as f32),
+        });
+        grid.push(Line {
+            start: (OFFSET_X, OFFSET_Y + CELL * i as f32),
+            end: (OFFSET_X + CELL * SIZE as f32, OFFSET_Y + CELL * i as f32),
+        });
+    }
+    grid
+}
+
+fn draw_grid(grid: &Vec<Line>) {
+    for l in grid {
+        draw_line(l.start.0, l.start.1, l.end.0, l.end.1, 1.0, DARKGRAY)
     }
 }
 
@@ -123,35 +136,43 @@ fn draw_pause() {
     draw_text("Press enter to continue!", 100.0, 300.0, 50.0, WHITE);
 }
 
-#[macroquad::main("MyGame")]
-async fn main() {
-    request_new_screen_size(720.0, 720.0);
-    let mut start: bool = true;
-    let mut pause: bool = false;
+fn new_game() -> (Snake, Fruit) {
+    let fruit: Fruit = Fruit {
+        pos: (10, 16),
+        draw_pos: (OFFSET_X + CELL * 16.0, OFFSET_Y + CELL * 10.0),
+        pts: 5,
+    };
 
-    let mut snake: Snake = Snake {
+    let snake: Snake = Snake {
         snake: VecDeque::from(vec![(28, 16), (29, 16), (30, 16)]),
         head: (28, 16),
         dir: Direction::UP,
         pts: 0,
         moves: Vec::new(),
     };
+    (snake, fruit)
+}
+
+#[macroquad::main("Snake")]
+async fn main() {
+    request_new_screen_size(720.0, 720.0);
+    let time = 0.1;
+    let mut last = get_time();
+
+    let mut start: bool = true;
+    let mut pause: bool = false;
+
+    let mut fruit: Fruit;
+    let mut snake: Snake;
+    (snake, fruit) = new_game();
+
+    let grid: Vec<Line> = build_grid();
 
     loop {
-        clear_background(BLACK);
-        draw_map();
-        snake.draw_snake();
-        if start {
-            draw_start();
-        } else if pause {
-            draw_pause();
-        } else {
-            snake.move_snake();
-        }
-
         if start {
             if is_key_pressed(KeyCode::Enter) {
                 start = false;
+                (snake, fruit) = new_game();
             }
         } else if pause {
             if is_key_pressed(KeyCode::Enter) {
@@ -170,6 +191,37 @@ async fn main() {
             } else if is_key_pressed(KeyCode::Down) || is_key_pressed(KeyCode::S) {
                 snake.turn_snake(Direction::DOWN);
             }
+
+            if get_time() - last > time {
+                last = get_time();
+                match snake.move_snake() {
+                    None => {
+                        println!("Game ended. Your points were {}.", snake.pts);
+                        start = true;
+                    }
+                    Some(_x) => {
+                        if snake.head == fruit.pos {
+                            snake.add_points(&fruit.pts);
+                            fruit.change_position((16, 16));
+                        } else {
+                            snake.snake.pop_back();
+                        }
+                    }
+                }
+            }
+        }
+
+        clear_background(BLACK);
+        draw_grid(&grid);
+        let txt = format!("Points: {}", snake.pts);
+        draw_text(&txt, 270.0, 40.0, 50.0, BLUE);
+        snake.draw_snake();
+        fruit.draw_fruit();
+
+        if start {
+            draw_start();
+        } else if pause {
+            draw_pause();
         }
         next_frame().await
     }
